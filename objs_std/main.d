3370361c8b944ff9a93477c3fd6892ae622fbd5f
Emmanuel TRAN <emtran@e1r2p5.clusters.42paris.fr>
2023-01-12 18:51:02 +0100

    ReverseIt : faire comparator

--- objs_std/main.d
+++
@@ -1,6 +0,0 @@
-objs_std/main.o: srcs/main.cpp srcs/../includes/test.hpp \
-  includes/colors.hpp
-
-srcs/../includes/test.hpp:
-
-includes/colors.hpp:
--- objs_std/test_std_functions.d
+++
@@ -1,6 +0,0 @@
-objs_std/test_std_functions.o: srcs/test_std_functions.cpp \
-  srcs/../includes/test.hpp includes/colors.hpp
-
-srcs/../includes/test.hpp:
-
-includes/colors.hpp:
